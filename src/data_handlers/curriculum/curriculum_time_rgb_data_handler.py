Singular scaling (Fix #86)

src/data_handlers/curriculum/curriculum_time_rgb_data_handler.py
@@ -31,7 +31,7 @@ class CurriculumBaselineRGBDataset(BaseHandler):
             response_type=response_type,
             is_train=is_train,
             subset_type=subset_type,
-            y_scaler=None,
+            y_scaler=y_scaler,
             results_dir=results_dir,
             use_saved_scaler=use_saved_scaler,
             prediction_step=prediction_step,
@@ -40,20 +40,11 @@ class CurriculumBaselineRGBDataset(BaseHandler):
             is_classification=is_classification,
             class_epsilon=class_epsilon,
         )
-        # Set y_scaler
-        self.y_scaler = y_scaler
-
         # Transform to torch Tensor
         self.X: torch.Tensor = torch.from_numpy(self.X)
         self.curr_X = self.X
-
-        # Apply the y-scaler to the target data
-        if self.y_scaler is not None or self.use_saved_scaler:
-            self.curr_Y = self.y_scaler.fit_transform(self.Y)
-        else:
-            self.curr_Y = self.Y
-        # Transform to torch Tensor
-        self.curr_Y = torch.from_numpy(self.curr_Y)
+        
+        self.curr_Y = self.Y
 
         self.subseq_len: int = subseq_len
         self.dataset_len: int = self.dataset_len - self.subseq_len
@@ -92,7 +83,7 @@ class CurriculumBaselineRGBDataset(BaseHandler):
         pass
 
     def update_y(self, **kwargs):
-        # Copy the original target data
+        # Load original scaled data
         self.curr_Y = self.Y
         if "sigma" in kwargs:
             # Apply gaussian smoothing to the target data
@@ -105,27 +96,25 @@ class CurriculumBaselineRGBDataset(BaseHandler):
 
             self.curr_Y = apply_gaussian_smoothening(self.curr_Y, sigma)
             # self.curr_Y = apply_asymmetric_gaussian_smoothening(self.curr_Y, sigma)
-
-            # TODO: Remove this plot after debugging
-            # fig, ax = plt.subplots(figsize=(20, 6))
-            # ax.plot(self.curr_Y[1], label='gaussian filtered')
-            # ax.set_title(f"Gaussian filtered channel 1")
-            # plt.legend()
-            # plt.savefig(f"gaussian_filtered_{sigma}.jpg", format="jpg", dpi=300)
-            # plt.close()
-        # Apply the y-scaler to the target data
-        if self.y_scaler is not None:
-            self.curr_Y = self.y_scaler.fit_transform(self.curr_Y)
-
-        # Transform to torch Tensor
-        self.curr_Y = torch.from_numpy(self.curr_Y)
+        
+        # TODO: Remove this plot after debugging
+        # import matplotlib.pyplot as plt
+        # fig, ax = plt.subplots(figsize=(20, 6))
+        # ax.plot(self.curr_Y[0], label='gaussian filtered')
+        # ax.set_title(f"Gaussian filtered channel 0")
+        # plt.legend()
+        # plt.savefig(f"gaussian_filtered_{sigma}.jpg", format="jpg", dpi=300)
+        # plt.close()
 
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
         x = self.curr_X[idx : idx + self.subseq_len]
         # Apply any transformations
         x = self.transform_x(x)
         # Get the target for the fourth image
-        y = self.curr_Y[:, idx + self.subseq_len - 1 + self.prediction_step]
+        y = torch.tensor(
+            self.curr_Y[:, idx + self.subseq_len - 1 + self.prediction_step],
+            dtype=torch.float32,
+        )
 
         return x, y
 

src/predictors/rnns/simple_cfc.py
@@ -17,9 +17,9 @@ class SimpleCFC(Predictor):
             mixed_memory=True,
             return_sequences=False,
         )
-        self.activation = nn.Sigmoid()
+        self.activation = nn.ReLU()
 
     def forward(self, x):
         x, _ = self.cfc(x)
-        # x = self.activation(x)
+        x = self.activation(x)
         return x

src/test.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     )
 
     # Define loss function
-    loss_fn = nn.BCEWithLogitsLoss()
+    loss_fn = nn.MSELoss()
 
     # Create metric tracker
     metrics_tracker = get_metric_tracker(config.testing.metrics)

src/utils/training_utils.py
@@ -114,8 +114,8 @@ def test_model(
             targets = labels.to(device)
             outputs = model(images)
 
-            probs = torch.sigmoid(outputs)
-            binary_outputs = (probs > 0.5).float()
+            # probs = torch.sigmoid(outputs)
+            # binary_outputs = (probs > 0.5).float()
 
             loss = loss_fn(outputs, targets)
             pbar.set_description(f"Test loss: {str(loss.item())}")
@@ -124,7 +124,7 @@ def test_model(
 
             if save_outputs_and_targets:
                 outputs_df = pd.concat(
-                    [outputs_df, pd.DataFrame(binary_outputs.cpu().numpy())]
+                    [outputs_df, pd.DataFrame(outputs.cpu().numpy())]
                 )
                 targets_df = pd.concat(
                     [targets_df, pd.DataFrame(targets.cpu().numpy())]