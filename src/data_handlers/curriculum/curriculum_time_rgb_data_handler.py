Curriculum dataset scaling bug fixes

src/data_handlers/curriculum/curriculum_time_rgb_data_handler.py
@@ -1,5 +1,6 @@
 import warnings
 import torch
+import numpy as np
 from typing import Any, Literal, Tuple
 from pathlib import Path
 from interfaces.base_handler import BaseHandler
@@ -31,7 +32,7 @@ class CurriculumBaselineRGBDataset(BaseHandler):
             response_type=response_type,
             is_train=is_train,
             subset_type=subset_type,
-            y_scaler=None,
+            y_scaler=y_scaler,
             results_dir=results_dir,
             use_saved_scaler=use_saved_scaler,
             prediction_step=prediction_step,
@@ -40,20 +41,14 @@ class CurriculumBaselineRGBDataset(BaseHandler):
             is_classification=is_classification,
             class_epsilon=class_epsilon,
         )
-        # Set y_scaler
-        self.y_scaler = y_scaler
 
         # Transform to torch Tensor
         self.X: torch.Tensor = torch.from_numpy(self.X)
         self.curr_X = self.X
 
-        # Apply the y-scaler to the target data
-        if self.y_scaler is not None or self.use_saved_scaler:
-            self.curr_Y = self.transform_y(self.Y.copy())
-        else:
-            self.curr_Y = self.Y.copy()
+        curr_Y = np.copy(self.Y)
         # Transform to torch Tensor
-        self.curr_Y = torch.from_numpy(self.curr_Y).to(torch.float32)
+        self.curr_Y = torch.from_numpy(curr_Y).to(torch.float32)
 
         self.subseq_len: int = subseq_len
         self.dataset_len: int = self.dataset_len - self.subseq_len
@@ -91,11 +86,11 @@ class CurriculumBaselineRGBDataset(BaseHandler):
 
     def update_y(self, **kwargs) -> None:
         # Copy the original target data
-        curr_Y = self.Y.copy()
+        curr_Y = np.copy(self.Y)
         if "sigma" in kwargs:
             # Apply gaussian smoothing to the target data
             sigma = kwargs["sigma"]
-            # TODO workaround for circular import
+            # Workaround for circular import
             from utils import (
                 apply_gaussian_smoothening,
                 # apply_asymmetric_gaussian_smoothening,
@@ -104,25 +99,14 @@ class CurriculumBaselineRGBDataset(BaseHandler):
             curr_Y = apply_gaussian_smoothening(curr_Y, sigma)
             # self.curr_Y = apply_asymmetric_gaussian_smoothening(self.curr_Y, sigma)
 
-            # TODO: Remove this plot after debugging
-            # fig, ax = plt.subplots(figsize=(20, 6))
-            # ax.plot(self.curr_Y[1], label='gaussian filtered')
-            # ax.set_title(f"Gaussian filtered channel 1")
-            # plt.legend()
-            # plt.savefig(f"gaussian_filtered_{sigma}.jpg", format="jpg", dpi=300)
-            # plt.close()
-        # Apply the y-scaler to the target data
-        if self.y_scaler is not None:
-            curr_Y = self.transform_y(curr_Y)
-
         # Transform to torch Tensor
         self.curr_Y = torch.from_numpy(curr_Y).to(torch.float32)
 
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
         x = self.curr_X[idx : idx + self.subseq_len]
-        # Apply any transformations
+        # Apply any transformations for input data
         x = self.transform_x(x)
-        # Get the target for the fourth image
+        # Get the target
         y = self.curr_Y[:, idx + self.subseq_len - 1 + self.prediction_step]
 
         return x, y