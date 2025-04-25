34a53ea9eca042d58043b9457a54a634e3f19934
JankowskiDaniel <jankowskidaniel06@gmail.com>
2025-04-25 18:32:36 +0000

    Added SimpleLTC predictor (Fix #112)

--- src/predictors/rnns/__init__.py
+++ src/predictors/rnns/__init__.py
@@ -1,4 +1,5 @@
 from predictors.rnns.single_lstm import SingleLSTM
 from predictors.rnns.simple_cfc import SimpleCFC
+from predictors.rnns.simple_ltc import SimpleLTC
 
-__all__ = ["SingleLSTM", "SimpleCFC"]
+__all__ = ["SingleLSTM", "SimpleCFC", "SimpleLTC"]
